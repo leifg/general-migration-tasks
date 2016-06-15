1513b791bc8ee84305a46932293c296830712967
Leif Gensert <leif@leif.io>
2016-06-15 21:20:28 +0200

    implement tasks that lets you create arbitrary sqlite databases

--- create-sqlite-from-csv/create-sqlite.rb
+++ create-sqlite-from-csv/create-sqlite.rb
@@ -1,1 +1,24 @@
-puts ARGV.map{|a| Dir.glob(a)}.flatten.inspect
+require "sequel"
+require "sqlite3"
+require "csv"
+
+def setup_table(database, table_name, headers)
+  database.create_table!(table_name) do
+    headers.each do |column|
+      String column
+    end
+  end
+end
+
+db_filename = ENV["DB_FILE_NAME"]
+db = Sequel.connect("sqlite://#{db_filename}")
+
+ARGV.each do |file|
+  table_name = File.basename(file,File.extname(file)).to_sym
+  csv_table = CSV.read(file, headers: true, header_converters: :symbol)
+  setup_table(db, table_name, csv_table.headers)
+  $stdout.puts("importing #{table_name}")
+  csv_table.each do |row|
+    db[table_name].insert(row.to_h)
+  end
+end
--- create-sqlite-from-csv/run.sh
+++ create-sqlite-from-csv/run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-printenv
-echo $HOME
-ruby create-sqlite.rb ../../csv-files/*.csv
+asset_path=$1
+output_db=$2
+csv_files=(${asset_path}/*.csv)
+
+export DB_FILE_NAME=$output_db
+
+bundle install &> /dev/null
+
+echo "Import into ${output_db}"
+bundle exec ruby create-sqlite.rb ${csv_files[@]}
